0e11ccf75981ca5df3d6b62a098f832b35076395
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-06-08 13:08:55 +0200

    lat long fixed

--- Open-weather-to-MongoDB/pyproducer.py
+++ Open-weather-to-MongoDB/pyproducer.py
@@ -1,17 +1,17 @@
+
 import os
 import requests
 from kafka.producer import KafkaProducer
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
-API_key='b8dcaf72ae27ff99a14719489bc647c1'
-lat=33.44
-lon=-94.04
-
+API_key = 'b8dcaf72ae27ff99a14719489bc647c1'
+lat = 49.414212
+lon = 8.709540
 # pylint: disable=C0103
 app = Flask(__name__)
 
 
-@app.route('/',methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def pyproducer():
     message = "No Post Request Recieved!"
     """Get Cloud Run environment variables."""
@@ -20,21 +20,22 @@ def pyproducer():
 
     url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API_key}"
     payload = {}
-    
+
     producer = KafkaProducer(bootstrap_servers="kafka-83d2d76-stud-9045.aivencloud.com:15768",
-                            security_protocol="SSL",
-                            ssl_cafile="ca.pem",
-                            ssl_certfile="service.cert",
-                            ssl_keyfile="service.key"
-                            )
+                             security_protocol="SSL",
+                             ssl_cafile="ca.pem",
+                             ssl_certfile="service.cert",
+                             ssl_keyfile="service.key"
+                             )
 
     response = requests.request("GET", url, data=payload)
-    producer.send(topic="weather",key=f"id".encode('utf-8'),value=response.text.encode('utf-8'))
+    producer.send(topic="weather", key=f"id".encode('utf-8'), value=response.text.encode('utf-8'))
 
     return render_template('index.html',
-            message=message,
-            Service=service,
-            Revision=revision)
+                           message=message,
+                           Service=service,
+                           Revision=revision)
+
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
