197a8de5fc4f193cd605d05dabaf0114dcbf096a
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-06-08 02:34:17 +0200

    auto reset fixed

--- Open-weather-to-MongoDB/pyconsumer.py
+++ Open-weather-to-MongoDB/pyconsumer.py
@@ -19,28 +19,35 @@ def main():
                           group_id=os.environ['CONSUMER_GROUP'],
                           value_deserializer=lambda m: json.loads(m.decode('utf-8')),
                           enable_auto_commit=False,
-                            auto_offset_reset='latest',
+                          auto_offset_reset='earliest',
                           security_protocol="SSL",
-                           ssl_cafile="ca.pem",
-                           ssl_certfile="service.cert",
+                          ssl_cafile="ca.pem",
+                        ssl_certfile="service.cert",
                            ssl_keyfile="service.key")
 
     consumer.subscribe([os.environ['TOPICS_NAME']])
 
 #tp = TopicPartition(os.environ['TOPICS_NAME'], 0)
 #consumer.assign([tp])
+
 #consumer.seek_to_end(tp)
 #lastOffset = consumer.position(tp)
 #consumer.seek_to_beginning(tp)
 
 
+    count=0
     for record in consumer:
         print(record.value)
         value = {}
-        value["_id"]=record.value["current"].pop("dt")
-        value.update(record.value["current"])
-        collection.insert_one(value)
-        print(value)
+        count+=1
+        try:
+            value["_id"]=record.value["current"].pop("dt")
+            value.update(record.value["current"])
+            print(value)
+            print(count)
+            collection.insert_one(value)
+        except:
+            continue
 
 if __name__ == '__main__':
     main()
